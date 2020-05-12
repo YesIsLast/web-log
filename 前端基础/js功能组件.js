8568a39dbc1249d1876820c29b76d7e7a8ea91eb
emmLi <601246372@qq.com>
2020-05-12 12:37:03 +0800

    js运算误差

--- 前端基础/js功能组件.js
+++ 前端基础/js功能组件.js
@@ -6,7 +6,7 @@ document.getElementById("sidebar-black").addEventListener('touchmove', function(
 function formatTofixed(num,n){
 	let regxp = new RegExp('([0-9]+\.[0-9]{' + n + '})[0-9]*')
 	return num.toString().replace(regxp, "$1")
-},
+}
 
 /**
  * 数组解析字符串
@@ -18,7 +18,7 @@ function getJsonStringify(list) {
 		result = result + item + ","
 	})
 	return result.substr(0, result.length - 1)
-},
+}
 /**
  * 字符串转数组
  * @param {Object} str 
@@ -28,7 +28,7 @@ function getJsonSplit(str) {
 		return str.split(",")
 	}
 	return []
-},
+}
 
 /**
  * js 日期格式化
@@ -85,3 +85,64 @@ function arrGroupBy(list, groupByName) {
 	}
 	return result
 }
+/**
+ * 加法
+ * @param arg1
+ * @param arg2
+ * @returns
+ */
+function accAdd(arg1,arg2){
+	var r1,r2,m;
+	try{r1=arg1.toString().split(".")[1].length}catch(e){r1=0};
+	try{r2=arg2.toString().split(".")[1].length}catch(e){r2=0};
+	m=Math.pow(10,Math.max(r1,r2));
+	return (arg1*m+arg2*m)/m;
+}
+
+/**
+* 减法
+* @param arg1
+* @param arg2
+* @returns
+*/
+function accSubtr(arg1,arg2){
+   var r1,r2,m,n;
+   try{r1=arg1.toString().split(".")[1].length;}catch(e){r1=0;}
+   try{r2=arg2.toString().split(".")[1].length;}catch(e){r2=0;}
+   m=Math.pow(10,Math.max(r1,r2));
+   //动态控制精度长度
+   n=(r1>=r2)?r1:r2;
+   return ((arg1*m-arg2*m)/m).toFixed(n);
+} 
+
+/***
+* 乘法，获取精确乘法的结果值
+* @param arg1
+* @param arg2
+* @returns
+*/
+function accMul(arg1,arg2)
+{
+ var m=0,s1=arg1.toString(),s2=arg2.toString();
+ try{m+=s1.split(".")[1].length}catch(e){};
+ try{m+=s2.split(".")[1].length}catch(e){};
+ return Number(s1.replace(".",""))*Number(s2.replace(".",""))/Math.pow(10,m);
+}
+
+/***
+* 除法，获取精确乘法的结果值
+* @param arg1
+* @param arg2
+* @returns
+*/
+function accDivCoupon(arg1,arg2){
+   var t1=0,t2=0,r1,r2;
+   try{t1=arg1.toString().split(".")[1].length;}catch(e){}
+   try{t2=arg2.toString().split(".")[1].length;}catch(e){}
+   with(Math){
+	   r1=Number(arg1.toString().replace(".",""));
+	   r2=Number(arg2.toString().replace(".",""));
+	   return (r1/r2)*pow(10,t2-t1);
+   }
+}
+
